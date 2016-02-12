2999527ea4a9ad5145df1747f78bcc20b92dd77a
Charles Forsyth <charles.forsyth@gmail.com>
2016-02-12 16:45:07 +0000

    nsec.c not actually used, but replace it by more sensible one

--- sys/src/libc/9sys/nsec.c
+++ sys/src/libc/9sys/nsec.c
@@ -16,61 +16,19 @@ be2vlong(vlong *to, uchar *f)
 		t[o[i]] = f[i];
 }
 
-static int fd = -1;
-static struct {
-	int	pid;
-	int	fd;
-} fds[64];
-
 vlong
 nsec(void)
 {
 	uchar b[8];
 	vlong t;
-	int pid, i, f, tries;
+	int f, n;
 
-	/*
-	 * Threaded programs may have multiple procs
-	 * with different fd tables, so we may need to open
-	 * /dev/bintime on a per-pid basis
-	 */
-
-	/* First, look if we've opened it for this particular pid */
-	if((pid = _tos->pid) == 0)		/* 9vx bug, perhaps? */
-		_tos->pid = pid = getpid();
-	do{
-		f = -1;
-		for(i = 0; i < nelem(fds); i++)
-			if(fds[i].pid == pid){
-				f = fds[i].fd;
-				break;
-			}
-		tries = 0;
-		if(f < 0){
-			/* If it's not open for this pid, try the global pid */
-			if(fd >= 0)
-				f = fd;
-			else{
-				/* must open */
-				if((f = open("/dev/bintime", OREAD|OCEXEC)) < 0)
-					return 0;
-				fd = f;
-				for(i = 0; i < nelem(fds); i++)
-					if(fds[i].pid == pid || fds[i].pid == 0){
-						fds[i].pid = pid;
-						fds[i].fd = f;
-						break;
-					}
-			}
-		}
-		if(pread(f, b, sizeof b, 0) == sizeof b){
-			be2vlong(&t, b);
-			return t;
-		}
+	t = 0;
+	if((f = open("/dev/bintime", OREAD|OCEXEC)) >= 0){
+		n = pread(f, b, sizeof(b), 0);
 		close(f);
-		if(i < nelem(fds))
-			fds[i].fd = -1;
-	}while(tries++ == 0);	/* retry once */
-	USED(tries);
-	return 0;
+		if(n == sizeof(b))
+			be2vlong(&t, b);
+	}
+	return t;
 }
